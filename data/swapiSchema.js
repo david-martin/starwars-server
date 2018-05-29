937ce472c9276007e6c7359d57f029270a8a93af
David Martin <david.martin@redhat.com>
2018-05-29 23:30:47 +0100

    extract schema to file

--- data/swapiSchema.js
+++ data/swapiSchema.js
@@ -19,97 +19,14 @@ import {
 } from 'graphql';
 
 import * as _ from "lodash";
-// import { Client } from "pg";
-// const client = new Client({
-//   connectionString: 'postgresql://postgres:mysecretpassword@localhost:5432/postgres'
-// })
-// client.connect();
-// client.query('SELECT NOW()', (err, res) => {
-//   console.log(err, res)
-// });
 
 import models from '../models/index.js';
 
 import { makeExecutableSchema } from 'graphql-tools';
+import * as fs from 'fs';
+import * as path from 'path';
 
-const schemaString = `
-schema {
-  query: Query
-  mutation: Mutation
-}
-
-# The query type, represents all of the entry points into our object graph
-type Query {
-  readNote(id: String): Note
-  listNotes: [Note]
-}
-
-# The mutation type, represents all updates we can make to our data
-type Mutation {
-  createNote(note: NoteInput): Note
-  updateNote(note: NoteInput): Note
-  deleteNote(note: NoteInput): Note
-}
-
-type Note {
-  id: String
-  title: String
-  content: String
-  createdAt: Float
-}
-
-input NoteInput {
-  id: String!
-  title: String
-  content: String
-  createdAt: Float
-}
-`;
-
-// const notes = [
-//   {
-//     id: '2000',
-//     title: 'C-3PO',
-//     content: 'note content',
-//     createdAt: 1527106784849
-//   },
-// ];
-
-// const resolvers = {
-//   Query: {
-//     readNote: (root, { id }) => {
-//       console.log('notes readNote', notes);
-//       return _.find(notes, {
-//         id: id
-//       });
-//     },
-//     listNotes: () => {
-//       return notes;
-//     }
-//   },
-//   Mutation: {
-//     createNote: (root, { note }) => {
-//       notes.push(note);
-//       console.log('notes createNote', notes);
-//       return note;
-//     },
-//     updateNote: (root, { note }) => {
-//       notes[_.findIndex(notes, {
-//         id: note.id
-//       })] = note;
-//       console.log('notes updateNote', notes);
-//       return note
-//     },
-//     deleteNote: (root, { note }) => {
-//       _.remove(notes, {
-//         id: id
-//       });
-//       console.log('notes deleteNote', notes);
-//       return note;
-//     },
-//   },
-// }
-
+const schemaString = fs.readFileSync(path.join(__dirname, 'schema.graphql')).toString();
 
 const resolvers = {
   Query: {
@@ -147,3 +64,5 @@ export const StarWarsSchema = makeExecutableSchema({
   typeDefs: [schemaString],
   resolvers
 });
+
+debugger
